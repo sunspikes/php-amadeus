14cd7cdc2d0a9914b3562baf66e1870857a160c5
Krishnaprasad MG <sunspikes@gmail.com>
2013-06-17 11:14:09 +0530

    Initial package commit.

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
  * Amadeus Sample client
  */
 
-namespace Example;
+include 'src/Amadeus/Client.php';
 
 use Amadeus\Client;
 
@@ -11,7 +11,7 @@ use Amadeus\Client;
 $ws = new Client('AmadeusWebServices.wsdl', true);
 
 // Authenticate
-$ws->securityAuthenticate([YOUR_SOURCE], [YOUR_ORIGIN], [YOUR_PASSWORD], [PASSWORD_LENGTH], [ORGANIZATION_ID]);
+$ws->securityAuthenticate('LONP02001', 'WS1APOC', 'MnJPRWJyaSZXb1V0', '12', '1A');
 
 // Travel from and to locations
 $from = 'DEL';
@@ -19,34 +19,34 @@ $to = 'BLR';
 
 // Travel Segments
 $segments[] = array(
-  'dep_date' => '230612', 
-  'dep_location' => 'DEL', 
-  'dest_location' => 'BLR',  
-  'company' => 'IT', 
-  'flight_no' => '201', 
-  'class' => 'Y', 
+  'dep_date' => '230612',
+  'dep_location' => 'DEL',
+  'dest_location' => 'BLR',
+  'company' => 'IT',
+  'flight_no' => '201',
+  'class' => 'Y',
   'passengers' => '2',
 );
 $segments[] = array(
-  'dep_date' => '250612', 
-  'dep_location' => 'BLR', 
-  'dest_location' => 'DEL',  
-  'company' => 'IT', 
-  'flight_no' => '202', 
-  'class' => 'Y', 
+  'dep_date' => '250612',
+  'dep_location' => 'BLR',
+  'dest_location' => 'DEL',
+  'company' => 'IT',
+  'flight_no' => '202',
+  'class' => 'Y',
   'passengers' => '2',
 );
 
 // Setup travellers
-$travellers['A'] = array( 
+$travellers['A'] = array(
   array(
-    'surname' => 'DOE', 
+    'surname' => 'DOE',
     'first_name' => 'JOHN'
   ),
 );
-$travellers['C'] = array( 
+$travellers['C'] = array(
   array(
-    'surname' => 'DWYNE', 
+    'surname' => 'DWYNE',
     'first_name' => 'JOHNSON'
   ),
 );
